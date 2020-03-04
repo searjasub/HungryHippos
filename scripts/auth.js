5addcfd282148a970f92026665a84648c9494559
LazerPlatypus(Laptop) <aholt@student.neumont.edu>
2020-03-04 14:18:23 -0700

    fixed My Account and Leaderboard

--- scripts/auth.js
+++ scripts/auth.js
@@ -2,11 +2,11 @@ exports.requireLogin = (req, res, callback) => {
     if (req.session.user) {
         return callback();
     } else {
-        let model = {
-            title: 'Error',
-            message: 'You must be logged in to view this page.'
-        };
-        res.redirect("/", model);
+        // let model = {
+        //     title: 'Error',
+        //     message: 'You must be logged in to view this page.'
+        // };
+        res.redirect("/");
         return;
     }
 }
@@ -15,11 +15,11 @@ exports.requireAdmin = (req, res, callback) => {
     if (req.session.user.isAdmin) {
         return callback();
     } else {
-        let model = {
-            title: 'Error',
-            message: 'You must do not have the previleges to view this page.'
-        };
-        res.redirect("/", model);
+        // let model = {
+        //     title: 'Error',
+        //     message: 'You must do not have the previleges to view this page.'
+        // };
+        res.redirect("/");
         return;
     }
 }
