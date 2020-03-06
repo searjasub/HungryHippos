16f283c4d2dfe4c5ac5ef6ad97f106b1d2930e32
Searjasub Lopez <slopez@student.neumont.edu>
2020-03-06 14:35:38 -0700

    flipped the hippos so they are diagonal

---
+++ scripts/game.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById("gameCanvas");
+
+let blue = "public/images/blue.png";
+let red = "public/images/red.png";
+let yellow = "public/images/yellow.png";
+let green = "public/images/green.png";
+
+let imgObj = new Image();
+imgObj.src = blue;
