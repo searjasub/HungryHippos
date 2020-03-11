1e487396aed4638e43b23850ee6ca27a96a568bd
tikalaka <baydadan001@gmail.com>
2020-03-11 13:05:03 -0600

    change ball spawns

--- public/keyDetection.js
+++ public/keyDetection.js
@@ -4,10 +4,10 @@
 class KeyDetection {
 
     static keyData = [{keyCode: 82, action: () => {alert("The W has Been Pressed")}},
-                      {keyCode: 67, action: () => {alert("The C has Been Pressed")}},
-                      {keyCode: 77, action: () => {alert("The M has Been Pressed")}},
-                      {keyCode: 80, action: () => {alert("The P has Been Pressed")}}
-                     ];
+                    {keyCode: 67, action: () => {alert("The C has Been Pressed")}},
+                    {keyCode: 77, action: () => {alert("The M has Been Pressed")}},
+                    {keyCode: 80, action: () => {alert("The P has Been Pressed")}}
+                    ];
 
     static init() {
         window.addEventListener("keydown", this.checkKeyPress, false);
